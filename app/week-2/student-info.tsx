bce87347907ebbc22e622e69bbfd73b29c3a45eb
Leke Adeyemo <lekke.dev@gmail.com>
2026-01-22 13:45:10 -0700

    Added week folder

---
+++ app/week-2/student-info.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h2>Adeleke Adeyemo</h2>
+
+      <Link href="https://github.com/Dev-Leke">Link to my Github account</Link>
+    </main>
+  );
+}
